a25ff3f8e0413ca10045638ca702d0673bbb1c34
WorldJournal-Lyu <lyu@worldjournal.com>
2018-06-08 16:19:29 -0400

    Added 3 new functions
    WebClient-UploadFile, WebClient-DownloadFile, WebRequest-UploadFile

--- WorldJournal.Ftp.psm1
+++ WorldJournal.Ftp.psm1
@@ -3,32 +3,53 @@
 WorldJournal.Ftp.psm1
 
     2018-06-05 Initial creation
+    2018-06-07 Added 'WebClient-UploadFile', 'WebClient-DownloadFile'
+    2018-06-08 Added 'WebRequest-UploadFile'
 
 #>
 
 
 function WebRequest-UploadFile {
-  param(
-    [Parameter(Mandatory=$true)][string]$Username,
-    [Parameter(Mandatory=$true)][string]$Password,
-    [Parameter(Mandatory=$true)][string]$RemoteFilePath,
-    [Parameter(Mandatory=$true)][string]$LocalFilePath
-  )
+    param(
+        [Parameter(Mandatory=$true)][string]$Username,
+        [Parameter(Mandatory=$true)][string]$Password,
+        [Parameter(Mandatory=$true)][string]$RemoteFilePath,
+        [Parameter(Mandatory=$true)][string]$LocalFilePath
+    )
 
-  $localFileContent = Get-Content $LocalFilePath -Encoding Byte
+    $localFileContent = Get-Content $LocalFilePath -Encoding Byte
 
-  $ftpWebRequest = [System.Net.FtpWebRequest]::Create($RemoteFilePath);
-  $ftpWebRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile;
-  $ftpWebRequest.UseBinary = $true
-  $ftpWebRequest.Credentials = New-Object System.Net.NetworkCredential($username,$password)
-  $ftpWebRequest.ContentLength = $localFileContent.Length;
+    $ftpWebRequest = [System.Net.FtpWebRequest]::Create($RemoteFilePath)
+    $ftpWebRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+    $ftpWebRequest.UseBinary = $true
+    $ftpWebRequest.Credentials = New-Object System.Net.NetworkCredential($Username,$Password)
+    $ftpWebRequest.ContentLength = $localFileContent.Length
 
-  $requestStream = $ftpWebRequest.GetRequestStream();
-  $requestStream.Write($localFileContent, 0, $localFileContent.Length);
-  $requestStream.Close();
+    try{
 
-  $response = $ftpWebRequest.GetResponse();
-  $response.Close();
+        $requestStream = $ftpWebRequest.GetRequestStream()
+        $requestStream.Write($localFileContent, 0, $localFileContent.Length)
+        $requestStream.Close()
+        $response = $ftpWebRequest.GetResponse()    
+
+        $returnParam = @{
+            Verb    = "Upload"
+            Noun    = ($response.StatusDescription).substring(0, ($response.StatusDescription).Length-3)
+            Status  = "Good"
+        }
+
+    }catch{
+
+        $returnParam = @{
+            Verb    = "Upload"
+            Noun    = $_.Exception.Message
+            Status  = "Bad"
+        }
+
+    }
+
+    $response.Close()
+    return $returnParam
 }
 
 
